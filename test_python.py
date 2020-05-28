08179b14247bf3ee3d9afc18f91cccc771aa7a40
Raul Patachi <raul.patachi@accesa.eu>
2020-05-28 21:23:19 +0300

    first commit

--- test_python.py
+++ test_python.py
@@ -1,1 +1,24 @@
-print("Hello world")
+print("Hello")
+
+one_variable = '5'
+some_string = 'Some other string'
+
+third_variable = one_variable + some_string
+
+some_number = 5
+some_other_number = 6
+
+result = some_number + some_other_number
+
+items = ['1', '2', '3', '5']
+
+for item in items:
+    print(item)
+
+
+number = [1, 2, 3, 4, 5, 6]
+
+for number in numbers:
+    
+    if number % 2 == 0:
+        print(number)
